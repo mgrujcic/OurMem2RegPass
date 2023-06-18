9aba1074cad9b01fe25acd31c52189767efe04e9
Markic01 <01marko.radosavljevic@gmail.com>
2023-06-18 04:17:29 +0200

    Added test files and changed commands file accordingly

---
+++ test1.c
@@ -0,0 +1,8 @@
+int sum(int len)
+{
+    int s=0;
+    for(int i=0;i<len;i++) {
+        s += i;
+    }
+    return s;
+}
---
+++ test2.c
@@ -0,0 +1,7 @@
+int f(int len) {
+    int x;
+    if (len > 5) {
+        x = 10;
+    }
+    return x;
+}
